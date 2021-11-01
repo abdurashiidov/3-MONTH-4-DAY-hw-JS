f9265514a07bf6d96573012e10ab8dfeccbc49d9
abdurashiidov <abdurashiidov18@gmail.com>
2021-11-01 16:12:52 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var name = prompt("❕Ismingizni kiriting❕")
+var surname = prompt("❕Familiyangizni kiriting❕")
+var from = prompt("🏙Qayerdansiz?🏙")
+var year = prompt("📅Nechinchi yilda tug'ilgansiz?📅")
+var number = prompt("🤴Oilada nechtasizlar?👱‍♀️")
+var married = prompt("🤵👰Turmush qurganmisiz?🤵👰")
+var workorstudy = prompt("👨‍🎓👨‍🔧O'qiysizmi yoki ishlaysizmi?👨‍🎓👨‍🔧")
+var how = prompt("👨‍🚀👨‍💻Qayerda o'qiysiz yoki ishlaysiz?👨‍🚀👨‍💻")
+var future = prompt("🙋‍♂️Kelajakda kim bo'lmoxchisiz?🧏‍♂️")
+var interest = prompt("💵1mln pulingiz bo'lsa nima qilardingiz?💵")
+
+
+
+
+alert (name + " " + surname + "\n" + from + "\n" + year + "\n" + number + "\n" + married + "\n" + workorstudy + "\n" + how + "\n" + future + "\n" + interest)
+console.log(name + " " + surname + "\n" + from + "\n" + year + "\n" + number + "\n" + married + "\n" + workorstudy + "\n" + how + "\n" + future + "\n" + interest);
